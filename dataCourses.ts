1cb5f4ca0e22bb78ea9d8d7b7a6708cfadb64a58
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2021-04-11 18:16:09 -0500

    Taller terminado

---
+++ dataCourses.ts
@@ -0,0 +1,10 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de Sw", "César Forero", 3),
+  new Course("DAlgo", "Rodrigo Cardoso", 3),
+  new Course("Sistrans", "Claudia Jiménez", 3),
+  new Course("Proba", "Jimmy Castro", 3),
+  new Course("Vectorial", "Jairo Ángel", 6)
+]
